534687823c74b90980f8d53f146212eef73d725a
VexheslavSP <willy0@rambler.ru>
2023-02-09 16:56:30 +0300

    Выполнено 11 дз

--- Task_11.py
+++ Task_11.py
@@ -7,7 +7,7 @@ import numpy
 # f(x)=-12*x^4*sin(cos(x))-18*x^2+10*x-30
 # x = [-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 x = numpy.linspace(-15.00000, 15.000000, num=10000)
-full_graph = []
+
 func_increases = []
 func_decreasing = []
 extrem = []
@@ -16,18 +16,31 @@ func_below_zero = []
 func_above = find_answer.find_above_below_zero(x)[0]
 func_below = find_answer.find_above_below_zero(x)[1]
 data = find_answer.build(x)
-for element in x:
-    full_graph.append((-12*element ** 4*math.sin(math.cos(element)) -
-                       18*element ** 3+5*element**2+10*element-30))
 fig = plt.figure()
 # plt.plot(x, full_graph)
 extrem = find_answer.find_extrem(x, data)
 radix = find_answer.radix(x, data)
 tmp_arr_y = [0] * len(radix)
-plt.scatter(func_above[0], func_above[1])
-plt.scatter(func_below[0], func_below[1])
+plt.scatter(func_above[0], func_above[1], marker="D", s=1)
+plt.scatter(func_below[0], func_below[1], marker="D", s=1)
 plt.scatter(extrem[1], extrem[0])
 plt.scatter(radix, tmp_arr_y)
+growth_y = find_answer.find_growth(
+    x, data, extrem[0])[0]
+
+growth_x = find_answer.find_growth(x, data, extrem[0])[1]
+
+fall_y = find_answer.find_growth(x, data, extrem[0])[2]
+
+fall_x = find_answer.find_growth(x, data, extrem[0])[3]
+i = 0
+j = 0
+while (i < len(growth_y)):
+    plt.plot(growth_x[i], growth_y[i],  'y--')
+    i += 1
+while (j < len(fall_y)):
+    plt.plot(fall_x[j], fall_y[j],  'm--')
+    j += 1
 plt.grid(True)
 
 plt.show()
--- find_answer.py
+++ find_answer.py
@@ -1,5 +1,6 @@
 import math
 import numpy
+import matplotlib.pyplot as plt
 
 
 def find_above_below_zero(input_range):
@@ -68,3 +69,59 @@ def radix(input_range, data):
         else:
             i += 1
     return radix_x
+
+
+# графический способ решения- разбей на участки межу экстемумами
+def find_growth(input_range, data, extr_y):
+    tmp_line_grown_y = []
+    tmp_line_grown_x = []
+    tmp_line_fall_y = []
+    tmp_line_fall_x = []
+    line_grown_y = [[]]
+    line_grown_x = [[]]
+    line_fall_y = [[]]
+    line_fall_x = [[]]
+    i = 0
+    j = 0
+    k = 1
+    l = 0
+    m = 1
+    n = 0
+    while i in range(len(input_range)-1):
+        if (data[i] < extr_y[j]):
+            tmp_line_grown_y.append(data[i])
+            tmp_line_grown_x.append(input_range[i])
+            i += 1
+        elif (data[i] > extr_y[j]):
+            tmp_line_fall_y.append(data[i])
+            tmp_line_fall_x.append(input_range[i])
+            i += 1
+        if (data[i] == extr_y[j]):
+            if (j+1 < len(extr_y)):
+                j += 1
+            i += 1
+    while k in range(len(tmp_line_grown_y)-1):
+        # если разрыв меньше 2х точек
+        if (tmp_line_grown_x[k]-tmp_line_grown_x[k-1] < (input_range[1]-input_range[0])*2):
+
+            line_grown_y[l].append(tmp_line_grown_y[k])
+            line_grown_x[l].append(tmp_line_grown_x[k])
+            k += 1
+        else:
+            line_grown_y.append(list(''))
+            line_grown_x.append(list(''))
+            l += 1
+            k += 1
+    while m in range(len(tmp_line_fall_y)-1):
+        # если разрыв меньше 2х точек
+        if (tmp_line_fall_x[m]-tmp_line_fall_x[m-1] < (input_range[1]-input_range[0])*2):
+
+            line_fall_y[n].append(tmp_line_fall_y[m])
+            line_fall_x[n].append(tmp_line_fall_x[m])
+            m += 1
+        else:
+            line_fall_y.append(list(''))
+            line_fall_x.append(list(''))
+            n += 1
+            m += 1
+    return (line_grown_y, line_grown_x, line_fall_y, line_fall_x)
